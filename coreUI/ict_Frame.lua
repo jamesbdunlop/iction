409d060d59201771b6e0faadcfe10d141b63d9bd
jamesd <james@anim83d.com>
2017-03-29 00:24:51 +1100

    first pass at prep for clean up on the checkbox lists in the options panel

--- coreUI/ict_Frame.lua
+++ coreUI/ict_Frame.lua
@@ -3,6 +3,7 @@ local iction = iction
 iction.UIFrameElement = {}
 iction.UIButtonElement = {}
 iction.UISpellScrollFrameElement = {}
+iction.UICheckBoxListFrameElement = {}
 ------------------------------------------------------------------------------------------------------------------------
 --- FRAME
 function iction.UIFrameElement.create(self, data)
@@ -531,4 +532,230 @@ function iction.UISpellScrollFrameElement.setMoveScript(self)
     end)
 end
 
+------------------------------------------------------------------------------------------------------------------------
+--- CHECKBOX LIST UNIQUE SELECT SCROLL FRAME
+
+function iction.UICheckBoxListFrameElement.create(self, data)
+    self.frameName = data['nameAttr']
+    self.data = data
+    self.textures = {}
+    -- Create the frame --
+    self.scrollframe = CreateFrame("ScrollFrame", self.data['uiName'], self.data['uiParentFrame'] or UIParent, self.data['uiInherits'])
+    self.frame = CreateFrame("Frame", 'SpellList',  self.scrollframe)
+    self.scrollframe:SetScrollChild(self.frame)
+    self.scrollframe:SetWidth(self.data['w'])
+    self.scrollframe:SetHeight(self.data['h'])
+
+    self.setScrollPoints(self)
+    self.setPoints(self)
+
+    self.frame.isMoving = false
+    self.frame:EnableMouse(self.data['enableMouse'])
+    self.frame:SetClampedToScreen(self.data['SetClampedToScreen'])
+    self.frame:SetFrameStrata(self.data['strata'])
+    self.frame:SetWidth(self.data['w'])
+    self.frame:SetHeight(self.data['h'])
+    self.frame:SetAttribute('name', self.data['nameAttr'])
+    self.frame:SetBackdropColor(self.data['bgCol']['r'], self.data['bgCol']['g'], self.data['bgCol']['b'], self.data['bgCol']['a'])
+    -- Texture
+    self.textures = {}
+    self.createTextures(self)
+    self.frame.texture = self.textures[0]
+    -- Default fontString
+    self.frame:Show()
+    self.scrollframe:Show()
+end
+
+function iction.UICheckBoxListFrameElement.addItems(self, t)
+    -- format expected for table. The list index will become the number.
+    -- {[0]=nil,
+    --  [1] = {label = 'skin01'},
+    --  [2] = {label = 'skin02'}
+    -- }
+    self.checkBoxes = {}
+    local checkBoxList = iction.list_iter(t)
+    local y = 0
+    local h = 0
+    while true do
+        local checkBoxes, i = checkBoxList()
+        if not checkBoxes then break end
+        local checkBox = CreateFrame("CheckButton", "spellOptionsButton_"..spellData['id'], self.frame, "OptionsSmallCheckButtonTemplate")
+            if i == 1 then
+                checkBox:SetPoint("TOPLEFT", self.frame)
+            else
+                checkBox:SetPoint("TOPLEFT", self.frame, "TOPLEFT", 0, y)
+            end
+            checkBox:SetWidth(24)
+            checkBox:SetHeight(24)
+            checkBox:SetScript("OnClick", function(self)
+                    if self:GetChecked() then
+                        --Turn off all checkboxes
+                        for i=1, iction.table_length(self.checkBoxes) do
+                            -- do a label check here and only turn off the ones we don't want
+                            self.checkBoxes[0]:SetChecked(false)
+                        end
+                    end
+               end)
+            table.insert(self.checkboxes, checkBox)
+        -- THE LABEL
+        local fnt = self.frame:CreateFontString("checkBoxOptionsLabel_"..checkBoxes['label'], 'OVERLAY')
+            fnt:SetFont(iction.font, 14, 'OVERLAY', 'THICKOUTLINE')
+            fnt:SetFontObject("GameFontWhite")
+            fnt:SetTextColor(1,1, 1, 1)
+            fnt:SetText(checkBoxes['label'])
+            if i == 1 then
+                fnt:SetPoint("TOPLEFT", self.frame, 24, -4)  -- anchors the first string, parent should be the content frame
+            else
+                fnt:SetPoint("TOPLEFT", self.frame, "TOPLEFT", 24, y-4)
+            end
+        y = y -26
+        h = h +26
+    end
+    self.scrollframe:SetMaxResize(300, h)
+    self.frame:SetHeight(h)
+end
+
+function iction.UICheckBoxListFrameElement.createTextures(self, data)
+    local t = self.data['textures']
+    local itr = iction.list_iter(t)
+    while true do
+        local textureData = itr()
+        if textureData == nil then break end
+        local texture = self.frame:CreateTexture(textureData['name'], textureData['level'])
+        -- POINTS
+        if textureData['allPoints'] then
+            texture:SetAllPoints(textureData['allPoints'])
+        else
+            texture:SetPoint(textureData['anchorPoint'], textureData['apX'], textureData['apY'])
+        end
+
+        -- WIDTH / HEIGHT
+        if textureData['w'] then texture:SetWidth(textureData['w']) end
+        if textureData['h'] then texture:SetHeight(textureData['h']) end
+
+        -- TEXTURE PATH
+        texture:SetTexture(textureData['texture'])
+        texture:SetVertexColor(textureData['vr'], textureData['vg'], textureData['vb'], textureData['va'])
+
+        -- ADD TO THE TABLE
+        table.insert(self.textures, texture)
+    end
+end
+
+function iction.UICheckBoxListFrameElement.setScrollPoints(self)
+    self.scrollframe:ClearAllPoints()
+    --------------------------
+    --- Position Stuff
+    if ictionFramePos[self.frameName] then
+        self.data['pointPosition']['point'] = ictionFramePos[self.frameName]["point"]['point']
+        self.data['pointPosition']['relativePoint'] = ictionFramePos[self.frameName]["point"]['relativePoint']
+        self.data['pointPosition']['p'] = ictionFramePos[self.frameName]["point"]['p']
+        self.data['pointPosition']['x'] = ictionFramePos[self.frameName]["point"]['x']
+        self.data['pointPosition']['y'] = ictionFramePos[self.frameName]["point"]['y']
+    end
+
+    --- Setup the cache for postion
+    self.framePosition = {}
+    self.frameMoveParent = nil
+
+    local framePos = {point = self.data['pointPosition']['point'],
+                      relativeTo = self.data['pointPosition']["relativeTo"],
+                      relativePoint = self.data['pointPosition']['relativePoint'],
+                      xOfs = self.data['pointPosition']["x"], yOfs = self.data['pointPosition']["y"]}
+    self.storePos(self, 'startPos', framePos)
+    self.storePos(self, 'startPre', framePos)
+    self.storePos(self, 'endPos', framePos)
+    --- Set the inital position
+    self.scrollframe:SetPoint(self.data['pointPosition']['point'], self.data['pointPosition']["relativeTo"], self.data['pointPosition']["relativePoint"], self.data['pointPosition']["x"], self.data['pointPosition']["y"])
+    self.frameMoveParent = self.scrollframe:GetParent()
+    if iction.debugUI then print (self.frameName .. " |posx: " .. tostring(self.data['pointPosition']["x"]) .. " |posy: " .. tostring(self.data['pointPosition']["y"]) .. " |pos: " .. tostring(self.data['pointPosition']['point'])) end
+end
+
+function iction.UICheckBoxListFrameElement.setPoints(self)
+    self.frame:ClearAllPoints()
+    --------------------------
+    --- Position Stuff
+    if ictionFramePos[self.frameName] then
+        self.data['pointPosition']['point'] = ictionFramePos[self.frameName]["point"]['point']
+        self.data['pointPosition']['relativePoint'] = ictionFramePos[self.frameName]["point"]['relativePoint']
+        self.data['pointPosition']['p'] = ictionFramePos[self.frameName]["point"]['p']
+        self.data['pointPosition']['x'] = ictionFramePos[self.frameName]["point"]['x']
+        self.data['pointPosition']['y'] = ictionFramePos[self.frameName]["point"]['y']
+    end
+
+    --- Setup the cache for postion
+    self.framePosition = {}
+    self.frameMoveParent = nil
+
+    local framePos = {point = self.data['pointPosition']['point'],
+                      relativeTo = self.data['pointPosition']["relativeTo"],
+                      relativePoint = self.data['pointPosition']['relativePoint'],
+                      xOfs = self.data['pointPosition']["x"], yOfs = self.data['pointPosition']["y"]}
+    self.storePos(self, 'startPos', framePos)
+    self.storePos(self, 'startPre', framePos)
+    self.storePos(self, 'endPos', framePos)
+    --- Set the inital position
+    self.frame:SetPoint(self.data['pointPosition']['point'], self.data['pointPosition']["relativeTo"], self.data['pointPosition']["relativePoint"], self.data['pointPosition']["x"], self.data['pointPosition']["y"])
+    self.frameMoveParent = self.frame:GetParent()
+    if iction.debugUI then print (self.frameName .. " |posx: " .. tostring(self.data['pointPosition']["x"]) .. " |posy: " .. tostring(self.data['pointPosition']["y"]) .. " |pos: " .. tostring(self.data['pointPosition']['point'])) end
+end
+
+function iction.UICheckBoxListFrameElement.storePos(self, curPos, data)
+    self.framePosition[curPos] = data
+end
+
+function iction.UICheckBoxListFrameElement.setMovedPosition(self)
+    local deltaX = self.framePosition["startPre"]["xOfs"] - self.framePosition['endPos']["xOfs"]
+    local deltaY = self.framePosition["startPre"]["yOfs"] - self.framePosition['endPos']["yOfs"]
+    local parentFrame = self.frameMoveParent
+    if not self.framePosition["startPos"]['relativeTo'] then
+        parentFrame = UIParent
+    end
+
+    if iction.debuUI then print(self.frameName .. " set parent to ".. tostring(parentFrame:GetAttribute("name"))) end
+    self.frame:SetPoint(self.framePosition["startPos"]["point"],
+                        self.frameMoveParent,
+                        self.framePosition["startPos"]["relativePoint"],
+                        self.framePosition["startPos"]["xOfs"] - deltaX,
+                        self.framePosition["startPos"]["yOfs"] - deltaY)
+    --- Now set the global position for reload
+    ictionFramePos[self.frameName] = {}
+    ictionFramePos[self.frameName]["point"] = {}
+    ictionFramePos[self.frameName]["point"]['point'] = self.framePosition["startPos"]["point"]
+    ictionFramePos[self.frameName]["point"]['x']   = self.framePosition["startPos"]["xOfs"] - deltaX
+    ictionFramePos[self.frameName]["point"]['y']   = self.framePosition["startPos"]["yOfs"] - deltaY
+    ictionFramePos[self.frameName]["point"]['relativePoint']   = self.framePosition["startPos"]["relativePoint"]
+end
+
+function iction.UICheckBoxListFrameElement.setMoveScript(self)
+    local StartPoint, StartRelativeTo, StartRelativePoint, startXOfs, startYOfs
+    local prePoint, preRelativeTo, preRelativePoint, preXOfs, preYOfs
+    local postPoint, postRelativeTo, postRelativePoint, postXOfs, postYOfs
+    self.frame:SetScript("OnMouseDown", function(_, button)
+        if button == "LeftButton" then
+            StartPoint, StartRelativeTo, StartRelativePoint, startXOfs, startYOfs = self.frame:GetPoint(1)
+            if not StartRelativeTo or not StartRelativeTo:GetName() then
+                StartRelativeTo = iction_MF -- For some reason the UI can drop the parent frame entirely! so force the MF instead.
+            end
+            local framePos = {point = StartPoint, relativeTo = StartRelativeTo, relativePoint = StartRelativePoint, xOfs = startXOfs, yOfs = startYOfs}
+            self.storePos(self, 'startPos', framePos)
+
+            self.frame:StartMoving()
+
+            prePoint, preRelativeTo, preRelativePoint, preXOfs, preYOfs = self.frame:GetPoint(1)
+            local framePos = {point = prePoint, relativeTo = preRelativeTo, relativePoint = preRelativePoint, xOfs = preXOfs, yOfs = preYOfs}
+            self.storePos(self, 'startPre', framePos)
+        end
+    end)
+
+    self.frame:SetScript("OnMouseUp", function(_, button)
+        if button == "LeftButton" then
+            postPoint, postRelativeTo, postRelativePoint, postXOfs, postYOfs = self.frame:GetPoint(1)
+            local framePos = {point = postPoint, relativeTo = postRelativeTo, relativePoint = postRelativePoint, xOfs = postXOfs, yOfs = postYOfs}
+            self.storePos(self, 'endPos', framePos)
+            self.frame:StopMovingOrSizing()
+            self.setMovedPosition(self)
+        end
+    end)
+end
 
